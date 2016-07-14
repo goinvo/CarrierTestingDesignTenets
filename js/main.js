ca566709f8e569f3a3a1ee7ff102bb3adb29c18e
Beth Herlin <beth@goinvo.com>
2016-07-14 18:01:54 -0400

    sticky header still not working.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,20 @@
 $( document ).ready(function() {
-  
+
+  var wrap = $("#wrap");
+
+  wrap.on("scroll", function(e) {
+
+    if (this.scrollTop > 50) {
+      console.log("scrolled!");
+      wrap.addClass("fix-contact");
+    } else {
+      wrap.removeClass("fix-contact");
+    }
+
+  });
+
+  $('.contact').click(function(){
+    console.log("clicked!");
+  });
 
 });
