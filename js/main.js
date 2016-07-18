4b9acd1827ccc268f7975ccc1037d55bba22ac62
Beth Herlin <beth@goinvo.com>
2016-07-18 16:43:38 -0400

    button header fix

--- js/main.js
+++ js/main.js
@@ -1,20 +1,5 @@
 $( document ).ready(function() {
-  var $window = $(window),
-      $header = $('.header'),
-      $contactButton = $('#contact-button'),
-      $contactButtonContainer = $('#contact-button-container');
 
-  $window.scroll(function(e) {
-
-    if ($window.scrollTop() >= $contactButtonContainer.offset().top - 7) {
-      $contactButton.addClass("fix-contact");
-      $header.addClass('fix-contact');
-    } else {
-      $contactButton.removeClass("fix-contact");
-      $header.removeClass('fix-contact');
-    }
-
-  });
 
   $(function() {
 
@@ -43,12 +28,25 @@ $( document ).ready(function() {
       $('#results').html(html);
       $('.disease-count').html(patient.diseases.length);
 
+      var $window = $(window),
+          $header = $('.header'),
+          $contactButton = $('#contact-button'),
+          $contactButtonContainer = $('#contact-button-container');
+
+      $window.scroll(function(e) {
+
+        if ($window.scrollTop() >= $contactButtonContainer.offset().top - 7) {
+          $contactButton.addClass("fix-contact");
+          $header.addClass('fix-contact');
+        } else {
+          $contactButton.removeClass("fix-contact");
+          $header.removeClass('fix-contact');
+        }
+
+      });
+
     });
 
   });
-
-  var $element = $("#disease-list");
-  var numberOfChildren = $element.children().length;
-  console.log(numberOfChildren);
 
 });
