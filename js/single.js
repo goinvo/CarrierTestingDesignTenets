8819e4cabf0cb1eb1f707315793a8e20eed5b7fd
Beth Herlin <beth@goinvo.com>
2016-07-26 10:19:15 -0400

    got rid of scroll-dependant report header, other mobile adjustments

--- js/single.js
+++ js/single.js
@@ -43,26 +43,47 @@ $( document ).ready(function() {
       var html = Mustache.to_html(template, patient);
       $('#results').html(html);
       $('.disease-count').html(patient.diseases.length);
-
-      var $window = $(window),
-          $header = $('.header'),
-          $backButton = $('.back-button'),
-          $contactButton = $('#contact-button'),
-          $contactButtonContainer = $('#contact-button-container');
-
-      $window.scroll(function(e) {
-
-        if ($window.scrollTop() >= $contactButtonContainer.offset().top - 7) {
-          $contactButton.addClass("fix-contact");
-          $header.addClass('fix-contact');
-          $backButton.addClass('fix-contact');
-        } else {
-          $contactButton.removeClass("fix-contact");
-          $header.removeClass('fix-contact');
-          $backButton.removeClass('fix-contact');
-        }
-
-      });
+      //
+      // var $window = $(window),
+      //     $header = $('.header'),
+      //     $backButton = $('.back-button'),
+      //     $contactButton = $('#contact-button'),
+      //     $contactButtonContainer = $('#contact-button-container');
+      //
+      // $window.scroll(function(e) {
+      //
+      //   if ($window.scrollTop() >= $contactButtonContainer.offset().top - 7) {
+      //     $contactButton.addClass("fix-contact");
+      //     $header.addClass('fix-contact');
+      //     $backButton.addClass('fix-contact');
+      //   } else {
+      //     $contactButton.removeClass("fix-contact");
+      //     $header.removeClass('fix-contact');
+      //     $backButton.removeClass('fix-contact');
+      //   }
+      //
+      // });
+      //
+      // // media query event handler
+      // if (matchMedia) {
+      //   var mq = window.matchMedia("(min-width: 560px)");
+      //   mq.addListener(WidthChange);
+      //   WidthChange(mq);
+      // }
+      //
+      // // media query change
+      // function WidthChange(mq) {
+      //   if (mq.matches) {
+      //     $contactButton.removeClass("fix-contact");
+      //     $header.removeClass('fix-contact');
+      //     $backButton.removeClass('fix-contact');
+      //   } else {
+      //     $contactButton.addClass("fix-contact");
+      //     $header.addClass('fix-contact');
+      //     $backButton.addClass('fix-contact');
+      //   }
+      //
+      // }
 
     });
 
